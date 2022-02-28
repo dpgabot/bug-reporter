698ed525417dba1fce63d18efdb6158911cb57ed
nathanbaleeta <nbaleeta@gmail.com>
2022-02-28 15:08:04 +0300

    Removed duplicate form labels

--- pages/form.js
+++ pages/form.js
@@ -6,11 +6,11 @@ import BugFormComponent from '../components/BugFormComponent'
 export default function BasicGrid() {
     return (
         <Grid container spacing={2}>
-            <Grid item lg={4} />
-            <Grid item lg={8} xs={12}>
-                <BugFormComponent />
-            </Grid>
-            <Grid item lg={4} />
+        <Grid item lg={4} />
+        <Grid item lg={8} xs={12}>
+            <BugFormComponent />
+        </Grid>
+        <Grid item lg={4} />
         </Grid>
 
     );
