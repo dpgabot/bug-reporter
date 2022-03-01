f9bea9f11ec87c97f782d7035f6e7dca96595080
nathanbaleeta <nbaleeta@gmail.com>
2022-03-01 12:23:39 +0300

    Added minimum characters allowed for specific fields to validation schema

--- components/BugFormComponent.js
+++ components/BugFormComponent.js
@@ -213,11 +213,19 @@ const BugFormComponent = withFormik({
     },
 
     validationSchema: Yup.object().shape({
-        toDo: Yup.string().required("Required!"),
-        expectedResults: Yup.string().required("Required!"),
-        actualResults: Yup.string().required("Required!"),
+        toDo: Yup.string()
+            .required("Required!")
+            .min(5, "Too Short!"),
+        expectedResults: Yup.string()
+            .required("Required!")
+            .min(2, "Too Short!"),
+        actualResults: Yup.string()
+            .required("Required!")
+            .min(2, "Too Short!"),
         environment: Yup.array().min(1, 'Select atleast one environment used'),
-        stepsToReproduce: Yup.string().required("Required!"),
+        stepsToReproduce: Yup.string()
+            .required("Required!")
+            .min(2, "Too Short!"),
         email: Yup.string().email("Enter a valid email"),
         date: Yup.date().default(() => new Date()),
     }),
